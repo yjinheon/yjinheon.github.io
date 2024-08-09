12ad9b002d3f95f8d28c64404800d9632b6d02e2
jinheonyoon <yjinheon@gmail.com>
2024-08-09 22:56:12 +0900

    rebuilding site 2024. 08. 09. (금) 22:56:12 KST

--- extract_concept.py
+++ extract_concept.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import re
 
 
@@ -20,15 +21,6 @@ def extract_concept_blocks(file_path):
         return concept_blocks
 
 
-def extract_all_concept_blocks(directory):
-    all_concept_blocks = []
-    markdown_files = get_markdown_files(directory)
-    for file_path in markdown_files:
-        concept_blocks = extract_concept_blocks(file_path)
-        all_concept_blocks.extend(concept_blocks)
-    return all_concept_blocks
-
-
 def test_extract_concept_blocks():
     file_path = "/home/jinheonyoon/workspace/ml/Machine_Learning.md"
     concept_blocks = extract_concept_blocks(file_path)
@@ -39,6 +31,39 @@ def test_extract_concept_blocks():
             file.write(f"{block}\n\n")
 
 
+def extract_all_concept_blocks(directory):
+    all_concept_blocks = []
+    markdown_files = get_markdown_files(directory)
+    for file_path in markdown_files:
+        concept_blocks = extract_concept_blocks(file_path)
+        all_concept_blocks.extend(concept_blocks)
+    return all_concept_blocks
+
+
+def process_text(text):
+    pattern = r"- \*\*([\s\S]*?)\*\* : ([\s\S]*?) : ([\s\S]*?)(?=\n- \*\*|\Z)"
+
+    matches = re.findall(pattern, text)
+
+    data = []
+    for match in matches:
+        concept = match[0].strip()
+        description = match[1].strip()
+        tags = match[2].strip()
+        data.append([concept, description, tags])
+
+    return data
+
+
+def write_csv(data, filename="output.csv"):
+    with open(filename, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
+
+
+# Main function
+
+
 def main():
     # Directory containing markdown files
     markdown_directory = "content"
@@ -47,8 +72,18 @@ def main():
     all_concept_blocks = extract_all_concept_blocks(markdown_directory)
 
     # Print each concept block
-    for idx, block in enumerate(all_concept_blocks):
-        print(f"Concept Block {idx + 1}:\n{block}\n")
+    #    for idx, block in enumerate(all_concept_blocks):
+    #    print(f"Concept Block {idx + 1}:\n{block}\n")
+
+    all_concept_blocks = extract_all_concept_blocks(markdown_directory)
+
+    data = []
+    for block in all_concept_blocks:
+        data.extend(process_text(block))
+
+    write_csv(data, "output_test.csv")
+
+    print("\n\n".join(all_concept_blocks))
 
 
 if __name__ == "__main__":
